7cf216aa470112ad94e14f4f010932a9d07fcde3
Tomás Ruiz <tomastrg22@gmail.com>
2021-12-24 13:58:26 +0100

    chore(api,ui): update dependencies

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import ViteComponents from 'vite-plugin-components'
+import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
-  plugins: [vue(), ViteComponents()],
+  plugins: [vue(), Components()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src')
