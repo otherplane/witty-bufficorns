82cb843e6f6c5793a4c89b95d4662c202220e482
Tomás Ruiz <tomastrg22@gmail.com>
2022-01-14 17:44:31 +0100

    test(api): fix leaderboard tests

--- packages/api/test/src/domain/bufficorn.spec.ts
+++ packages/api/test/src/domain/bufficorn.spec.ts
@@ -1,27 +1,51 @@
 import { Bufficorn } from '../../../src/domain/bufficorn'
 import { Trait, BufficornLeaderboardInfo } from '../../../src/types'
+function auxBufficorn(index: number, stats: Array<number>): Bufficorn {
+  const b = new Bufficorn(undefined, index)
+  b.stats = {
+    vigor: stats[0],
+    agility: stats[1],
+    coat: stats[2],
+    coolness: stats[3],
+    speed: stats[4],
+    stamina: stats[5],
+  }
 
+  return b
+}
+
+function auxBufficornInfo(
+  b: Bufficorn,
+  position: number,
+  score: number
+): BufficornLeaderboardInfo {
+  return {
+    name: b.name,
+    ranch: b.ranch,
+    ...b.stats,
+    position,
+    score,
+  }
+}
 describe('bufficorn.ts', () => {
   it('calculateScore', async () => {
     const bufficorn = new Bufficorn(undefined, 0)
-    const emptyBufficorn = bufficorn
+
+    // Empty bufficorn
+    expect(bufficorn.calculateScore()).toStrictEqual(0)
+    expect(bufficorn.calculateScore(Trait.Vigor)).toStrictEqual(0)
 
     bufficorn.stats.vigor = 10
-    const onlyVigorBufficorn = bufficorn
+
+    // Bufficorn with only one valued trait
+    expect(bufficorn.calculateScore()).toStrictEqual(0)
+    expect(bufficorn.calculateScore(Trait.Vigor)).toStrictEqual(10)
 
     bufficorn.stats.agility = 20
     bufficorn.stats.coat = 30
     bufficorn.stats.coolness = 40
     bufficorn.stats.speed = 50
     bufficorn.stats.stamina = 60
-
-    // Empty bufficorn
-    expect(emptyBufficorn.calculateScore()).toStrictEqual(0)
-    expect(emptyBufficorn.calculateScore(Trait.Vigor)).toStrictEqual(0)
-
-    // Bufficorn with only one valued trait
-    expect(onlyVigorBufficorn.calculateScore()).toStrictEqual(0)
-    expect(onlyVigorBufficorn.calculateScore(Trait.Vigor)).toStrictEqual(10)
 
     // Bufficorn with all valued traits
     expect(bufficorn.calculateScore()).toStrictEqual(10)
@@ -32,34 +56,6 @@ describe('bufficorn.ts', () => {
     expect(bufficorn.calculateScore(Trait.Speed)).toStrictEqual(50)
     expect(bufficorn.calculateScore(Trait.Stamina)).toStrictEqual(60)
   })
-
-  function auxBufficorn(index: number, stats: Array<number>): Bufficorn {
-    const b = new Bufficorn(undefined, index)
-    b.stats = {
-      vigor: stats[0],
-      agility: stats[1],
-      coat: stats[2],
-      coolness: stats[3],
-      speed: stats[4],
-      stamina: stats[5],
-    }
-
-    return b
-  }
-
-  function auxBufficornInfo(
-    b: Bufficorn,
-    position: number,
-    score: number
-  ): BufficornLeaderboardInfo {
-    return {
-      name: b.name,
-      ranch: b.ranch,
-      ...b.stats,
-      position,
-      score,
-    }
-  }
 
   it('getLeaderboard', async () => {
     const bufficorn_0 = auxBufficorn(0, [10, 20, 30, 40, 50, 60])
@@ -86,10 +82,11 @@ describe('bufficorn.ts', () => {
     const expectedByStamina_0 = auxBufficornInfo(bufficorn_0, 0, 60)
     const expectedByStamina_1 = auxBufficornInfo(bufficorn_1, 2, 11)
     const expectedByStamina_2 = auxBufficornInfo(bufficorn_2, 1, 22)
+
     expect(sortedBufficornsByStamina).toStrictEqual([
       expectedByStamina_0,
-      expectedByStamina_1,
       expectedByStamina_2,
+      expectedByStamina_1,
     ])
   })
 })
