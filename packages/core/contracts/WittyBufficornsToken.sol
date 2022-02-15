45ae12cf10bbc7e34a8fa3b99a1debc45df90e7d
Guillermo Díaz <guillermo@otherplane.com>
2022-02-15 19:30:08 +0100

    fix(core): fix ranking validation on best bufficorn awards

--- packages/core/contracts/WittyBufficornsToken.sol
+++ packages/core/contracts/WittyBufficornsToken.sol
@@ -381,7 +381,7 @@ contract WittyBufficornsToken
             _tokenInfo.award = _farmerAwards[_ix];
             require(
                 uint8(_tokenInfo.award.category) < uint8(WittyBufficornsLib.Awards.BestBufficorn)
-                    || _tokenInfo.award.ranking > 3,
+                    || _tokenInfo.award.ranking <= 3,
                 "WittyBufficornsToken: bufficorn bad ranking"
             );
             __doSafeMint(_tokenOwner, _tokenInfo);
